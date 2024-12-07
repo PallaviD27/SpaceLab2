d4b2ea3f08a922735ec0b9edfc5f703d3260bfb0
pallavi <pallavi.dhingra27@gmail.com>
2024-12-07 20:47:41 +0100

    ANT part

--- ANT/ANT.py
+++ ANT/ANT.py
@@ -6,6 +6,8 @@ import os
 
 # Importing functions created in GenerateBits.py
 from Transmitter.GenerateBits import generate_bits
+from Transmitter.ChannelCoding import encode_hamming
+from Transmitter.ChannelCoding import generate_parity_check_matrix
 
 # Importing functions created in Channel.py
 from Channel import Channel
@@ -13,6 +15,26 @@ from Channel import Channel
 # Importing functions created in Receiver.py
 from Receiver import Receiver
 
-bits = generate_bits(32.32,False)
+
+try:
+    n_bits = float(input("Enter input bit length: "))  # Read input as a float
+    if n_bits.is_integer():  # Check if the number is actually an integer
+        n_bits = int(n_bits)  # Convert to int
+    else:
+        raise ValueError("The number of bits should be an integer")
+except ValueError as e:
+    print(f"Error: {e}")
+
+bits = generate_bits(n_bits,False)
 
 print(bits)
+
+parity_check_matrix=generate_parity_check_matrix()
+
+Code_word = encode_hamming(bits,parity_check_matrix)
+
+print(f"Code word: {Code_word}")
+
+Code_length = len(Code_word)
+
+print(f"Length of the code word:{Code_length} ")
--- ANT/Transmitter/ChannelCoding.py
+++ ANT/Transmitter/ChannelCoding.py
@@ -1,7 +1,6 @@
-# Channel Encode
+# Channel Encoder
 
 import numpy as np
-from ANT import bits
 
 
 # Parity check matrix generation
@@ -9,18 +8,18 @@ from ANT import bits
 
 '''This function generates a fixed parity-check matrix for (7,4) Hamming code.'''
 def generate_parity_check_matrix():
-    parity_check_mat = np.array([[0,1,1,1,1,0,0],
+    parity_check_mat1 = np.array([[0,1,1,1,1,0,0],
                                  [1,0,1,1,0,1,0],
                                  [1,1,0,1,0,0,1]])
-    return parity_check_mat
+    return parity_check_mat1
 
 # Generate the parity-check matrix and assign it to a variable
 # Also check order of the parity check matrix
-parity_check_matrix = generate_parity_check_matrix()
-order_parity_check = parity_check_matrix.shape
+parity_check_matrix1 = generate_parity_check_matrix()
+order_parity_check1 = parity_check_matrix1.shape
 
 # Print the parity check matrix
-print(f'The parity check matrix of order {order_parity_check}: \n {parity_check_matrix}')
+print(f'The parity check matrix of order {order_parity_check1}: \n {parity_check_matrix1}')
 
 '''End'''
 
@@ -29,7 +28,7 @@ print(f'The parity check matrix of order {order_parity_check}: \n {parity_check_
 
 '''This function generates the generator matrix based on the parity-check matrix'''
 def generate_generator_matrix(parity_check_matrix):
-    parity=parity_check_matrix[:,:4]
+    parity=parity_check_matrix1[:,:4]
 
     parity_transpose=parity.transpose()
 
@@ -40,9 +39,9 @@ def generate_generator_matrix(parity_check_matrix):
     return generator_mat
 
 # Generating the generator matrix
-generator_matrix = generate_generator_matrix(parity_check_matrix)
+generator_matrix = generate_generator_matrix(parity_check_matrix1)
 
-verify = (np.dot(parity_check_matrix, generator_matrix.transpose()))%2
+verify = (np.dot(parity_check_matrix1, generator_matrix.transpose()))%2
 
 print(f'Only for verification of G.HTanspose = 0: \n {verify}')
 
@@ -57,14 +56,15 @@ print(f'The generator matrix of the order {order_generator_matrix}: \n {generato
 '''This function generates the codeword based on the input bits and the generator matrix'''
 
 def encode_hamming(bits,parity_check_matrix):
+    sets = len(bits)//4
+    print(sets)
+    CodeWord=[]
+    for i in range (sets):
+        seti=bits[i*4:(i+1)*4]
+        print(f"Set {i+1}: {seti}")
+        CodeSeti=(np.dot(seti,generator_matrix)%2).astype(int)
+        print(f"CodeSet {i+1}: {CodeSeti} ")
+        CodeWord.extend(CodeSeti.tolist())
+    return CodeWord
 
-
-
-
-
-
-
-
-
-
-# code=encode_hamming(bits,parity_check_matrix)
+'''End'''
